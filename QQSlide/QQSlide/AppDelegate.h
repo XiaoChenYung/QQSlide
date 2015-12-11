6b4c047400a980a6a9853748b143e2fe08c68cc0
yangxiaochen <18730222889@126.com>
2015-12-11 14:20:23 +0800

    初始化

---
+++ QQSlide/QQSlide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQSlide
+//
+//  Created by cyyz on 15/12/11.
+//  Copyright © 2015年 cyyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQSlide/QQSlide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQSlide
+//
+//  Created by cyyz on 15/12/11.
+//  Copyright © 2015年 cyyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
